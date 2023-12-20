bbbd6334883b0f42a2fa59ebfb0dcfd5e3287110
Vit <mayllby@gmail.com>
2023-12-20 18:14:07 +0300

    Добавил 'Словари'

--- main02.py
+++ main02.py
@@ -137,3 +137,34 @@
 #     print(t[i])
 
 # t[0] = 2 # данная команда на замену значения элемента выдаст ошибку, т.к. кортежи нельзя изменять
+
+
+# 00:18:25
+
+''' Словари '''
+
+''' Словари — неупорядоченные коллекции произвольных объектов с доступом по ключу.
+В списках в качестве ключа используется индекс элемента. 
+В словаре для определения элемента используется значение ключа (строка, число).'''
+
+# d = {}
+# d = dict()
+
+# d['q'] = 'qwerty'
+# print(d)
+
+# d['w'] = 'werty'
+# print(d['q'])
+
+# dictionary = {}
+# dictionary = {'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
+# print(dictionary['left']) # ← типы ключей могут отличаться
+# print(dictionary['up']) # ↑ типы ключей могут отличаться
+# dictionary['left'] = '⇐'
+# print(dictionary['left']) # ⇐
+# print(dictionary['type']) # KeyError: 'type'
+# del dictionary['left'] # удаление элемента
+
+# for (k,v) in dictionary.items():
+#     print('{}: {}'.format(item, dictionary[item]))
