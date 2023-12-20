40eb7228e6599337422d09d333ee2c4681d640a2
Vit <mayllby@gmail.com>
2023-12-20 15:58:53 +0300

    удаление последнего элемента

--- main02.py
+++ main02.py
@@ -35,8 +35,21 @@
 
 ''' Алгоритм заполнения списка '''
 
-list_1 = []
-print(list_1)
-for i in range(5):
-    list_1.append(i)
-    print(list_1)
+# list_1 = []
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+
+
+# 00:08:00
+
+''' Удаление последнего элемента списка '''
+
+list_1 = [12, 7, -1, 21, 0]
+print(list_1.pop()) # 0
+print(list_1) # [12, 7, -1, 21]
+print(list_1.pop()) # 21
+print(list_1) # [12, 7, -1]
+print(list_1.pop()) # -1
+print(list_1) # [12, 7]
