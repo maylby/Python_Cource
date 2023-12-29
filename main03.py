7e4bacc150e7caa412258e96e9d31e5c320e0cbd
Vit <mayllby@gmail.com>
2023-12-29 21:02:25 +0300

    Добавил алгоритм сортировки слиянием

--- main03.py
+++ main03.py
@@ -232,3 +232,35 @@ P.S.
     и итоговый список будет выглядеть таким образом: 
     [2, 3] + [5] + [10] = [2, 3, 5, 10]
 """
+
+# 00:27:00
+
+""" Сортировка слиянием """
+
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+list1 = [1,4,9,7,2,1,44,55,3,6]
+merge_sort(list1)
+print(list1)
