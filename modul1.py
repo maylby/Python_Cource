fdbcaf381c7a9e3c7492e24f3fd0d22f60c11281
Vit <mayllby@gmail.com>
2023-12-29 19:15:00 +0300

    Добавил функцию Фибоначчи

--- modul1.py
+++ modul1.py
@@ -1,7 +1,18 @@
 """ Хранилище модулей кодов """
 
-# модуль max1 (выбор максимального числа)
+""" Функция 'max1' (выбор максимального числа) """
 
 def max1(a, b):
     if a > b: return a
     return b
+
+""" Функция 'fib' (последовательность Фибоначчи) """
+
+def fib(n):
+    if n == 0: return 0
+    if n in [1, 2]: return 1
+    return fib(n-1) + fib(n-2)
+
+list_1 = []
+for i in range(0, n):
+    list_1.append(fib(i))
