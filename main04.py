f26f92f2bfe168c7a209176d8e83a45640878049
Vit <mayllby@gmail.com>
2024-01-04 15:04:27 +0300

    Добавил 'калькулятор'

--- main04.py
+++ main04.py
@@ -17,14 +17,31 @@
 
 # 00:02:00
 
-x = int(input('Input variable X: ', ))
-def f(x):
-    return x*x
-a = f # замена 'f' на 'a'
-print(type(a))  # проверка типа данных
-                # выдаёт запись - class 'function', т.е.
-                # обе переменные 'f' и 'a' после передачи значения 'f' 
-                # ссылаются на какую-то оперативную память ПК,
-                # где записана данная функция
-print(a(x))
-print(f(x))
+# x = int(input('Input variable X: ', ))
+# def f(x):
+#     return x*x
+# a = f # замена 'f' на 'a'
+# print(type(a))  # проверка типа данных
+#                 # выдаёт запись - class 'function', т.е.
+#                 # обе переменные 'f' и 'a' после передачи значения 'f' 
+#                 # ссылаются на какую-то оперативную память ПК,
+#                 # где записана данная функция
+# print(a(x))
+# print(f(x))
+
+
+
+# 00:04:00
+
+""" Калькулятор """
+
+def calk1(a, b):
+    return a + b
+
+def calk2(a, b):
+    return a * b
+
+def math(op, x, y):
+    print(op(x, y))
+
+math(calk2, 5, 6)
