8aa8180bd01c7a2187cdc89833a2046fa244cab3
Vit <mayllby@gmail.com>
2024-01-04 20:11:08 +0300

    Добавил 'Функцию enumerate'

--- main04.py
+++ main04.py
@@ -242,15 +242,28 @@ lambda x: x % 2 == 0
 
 ''' Пример: '''
 
-users = ['user1', 'user2', 'user3', 'user4', 'user5']
-ids = [4, 5, 9, 14, 7]
-data = list(zip(users, ids))
-print(data) # [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# data = list(zip(users, ids))
+# print(data) # [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
 
-''' Функция zip () пробегает по минимальному входящему набору: '''
+# ''' Функция zip () пробегает по минимальному входящему набору: '''
 
-users = ['user1', 'user2', 'user3', 'user4', 'user5']
-ids = [4, 5, 9, 14, 7]
-salary = [111, 222, 333]
-data = list(zip(users, ids, salary))
-print(data) # [('user1', 4, 111), ('user2', 5, 222), ('user3', 9, 333)]
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(users, ids, salary))
+# print(data) # [('user1', 4, 111), ('user2', 5, 222), ('user3', 9, 333)]
+
+
+# 00:27:55
+
+""" Функция enumerate """
+"""
+Функция enumerate() применяется к итерируемому объекту и возвращает новый итератор с
+кортежами из индекса и элементов входных данных.
+Функция enumerate() позволяет пронумеровать набор данных. 
+"""
+users = ['user1', 'user2', 'user3']
+data = list(enumerate(users))
+print(data) # [(0, 'user1'), (1, 'user2'), (2, 'user3))]
