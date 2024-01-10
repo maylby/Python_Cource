c84ed6a281a3a9bb832825da2cef1df5c33d3bb8
Vit <mayllby@gmail.com>
2024-01-10 18:54:30 +0300

    Добавил функцию

--- modul1.py
+++ modul1.py
@@ -10,3 +10,23 @@ def max1(a, b):
 def min1(a, b):
     if a < b: return a
     return b
+
+
+def guick_sort(array):
+
+    if len(array) <= 1: return array
+    else: pivot = array[0]
+
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return guick_sort(less) + [pivot] + guick_sort(greater)
+
+
+def guick_sort_back(array):
+
+    if len(array) <= 1: return array
+    else: pivot = array[0]
+
+    less = [i for i in array[1:] if i > pivot]
+    greater = [i for i in array[1:] if i <= pivot]
+    return guick_sort_back(less) + [pivot] + guick_sort_back(greater)
